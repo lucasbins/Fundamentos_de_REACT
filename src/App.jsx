b401bf66399235865387a471d4d56dd165afa39f
lucasbins <lucas.braga@rede.ulbra.br>
2022-03-21 20:29:28 -0300

    Aula 24 - componente card #03

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "./App.css"
+
 import Primeiro from "./components/basicos/Primeiro"
 import ComParametro from "./components/basicos/ComParametro"
 import Fragmento from "./components/basicos/Fragmento"
@@ -8,27 +10,31 @@ import Card from "./components/layout/Card"
 
 export default function App(props) {
     return (
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos de REACT</h1>
-            <Card title="#04 - Aleatorio">
-                <Aleatorio max={100} min={0} />
-            </Card>
 
-            <Card title="#03 - Fragmento">
-                <Fragmento />
-            </Card>
+            <div className="Cards">
+                <Card title="#04 - Aleatorio">
+                    <Aleatorio max={100} min={0} />
+                </Card>
 
-            <Card title="#02 - Com Parametro">
-                <ComParametro
-                    title="Situacao do Aluno"
-                    aluno="Lucas"
-                    nota={9.3}
-                />
-            </Card>
+                <Card title="#03 - Fragmento">
+                    <Fragmento />
+                </Card>
 
-            <Card title="#01 - Primeiro">
-                <Primeiro></Primeiro>
-            </Card>
+                <Card title="#02 - Com Parametro">
+                    <ComParametro
+                        title="Situacao do Aluno"
+                        aluno="Lucas"
+                        nota={9.3}
+                    />
+                </Card>
+
+                <Card title="#01 - Primeiro">
+                    <Primeiro></Primeiro>
+                </Card>
+            </div>
+
         </div>
     )
 }
